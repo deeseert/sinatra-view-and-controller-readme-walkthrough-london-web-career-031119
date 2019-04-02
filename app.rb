ac53c860c9067d356f7cb04b3b11e8e802bdcd56
deeseert <gio.amms@gmail.com>
2019-04-02 17:36:22 +0100

    Done.

--- app.rb
+++ app.rb
@@ -6,11 +6,16 @@ class App < Sinatra::Base
   end
 
   post '/reverse' do
+    old_str = params[:string]
+    @new_str = old_str.reverse
+
     erb :reversed
   end
 
   get '/friends' do
     # Write your code here!
+  @friends = ['Emily Wilding Davison', 'Harriet Tubman', 'Joan of Arc', 'Malala Yousafzai', 'Sojourner Truth']
 
+  erb :friends
   end
 end
